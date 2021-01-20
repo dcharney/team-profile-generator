17b3eb4176e0e688b9ba2855da2567f5aa80e5e7
dcharney <delaneycharney@gmail.com>
2021-01-20 21:41:43 +0100

    all tests passing

--- src/html-template.js
+++ src/html-template.js
@@ -1,4 +1,3 @@
-const sampleData = require('../__tests__/sample-answers');
 const writeFile = require('./generate-html');
 
 class HTMLData {
@@ -63,7 +62,13 @@ class HTMLData {
 
 }
 
-
+sampleData = [
+    new Manager('Phil','001','phil@company.com','007'),
+    new Engineer('Delaney','102','delaney@company.com','dcharney'),
+    new Intern('Mike','304','mike@harvard.eu','Harvard'),
+    new Engineer('Steve','103','steve@company.com','sjcharney'),
+    new Engineer('Frank','103','frank@company.com','frankieFrankieFrankFrank')
+];
 
 const temp = new HTMLData(sampleData);
 
